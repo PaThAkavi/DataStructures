71856dc353252e5b9f7d68d3243f90a9f772e162
Avaneesh Pathak <pathakavaneesh@gmail.com>
2019-10-02 19:32:18 +0530

    Added new file for dynamic stack creation

---
+++ dynamicStack.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+bool isEmpty();
+bool isFull();
+void push(int);
+void pop();
+void peek();
+
+class Stack{
+    int totalSize;
+    int *stack;
+    int top;
+
+    public:
+
+    Stack(int size){
+        this->totalSize = size;
+        this->arr = new int[this->totalSize];
+        this->top = -1;
+    }
+
+    bool isEmpty(){
+        return this->totalSize = 0;
+    }
+
+    bool isFull(){
+        return this->top = this->totalSize-1;
+    }
+
+    void push(int ele){
+        if(isFull()){
+            cout << "OVERFLOW" <<endl;
+        }
+        else{
+            top += 1;
+            this->stack[this->top] = ele;
+        }
+    }
+
+    void pop(){
+        if(isEmpty()){
+            cout << "UNDERFLOW" << endl;
+        }
+        else{
+            int x;
+            x = this->stack[top];
+            top -= 1;
+        }
+    }
+
+    void peek(){
+        if(isEmpty()){
+            cout << "UNDERFLOW" <<endl;
+        }
+        else{
+            cout << "Top value is : " << this->stack[top] << endl;
+        }
+    }
+}
+
+int main(){
+    
+    return 0;
+}
+
